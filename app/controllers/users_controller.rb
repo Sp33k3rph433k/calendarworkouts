821b4c9e2fd20077a88f3ae1eaaa2632bc0dfe6f
Cameron Kidman <cameronjkidman@gmail.com>
2014-03-16 16:32:28 -0600

    Get the personal details form and edit working, paper_trail now functions properly with versioning info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,11 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def dashboard
-    @user = current_user
+    @personal_detail = current_user.personal_detail
   end
 
   def index
     @user = current_user
+    @personal_detail = @user.personal_detail
   end
 end
