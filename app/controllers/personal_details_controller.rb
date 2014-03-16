88157f6cb54f43a1c68461a368a9df75b75a35a8
Cameron Kidman <cameronjkidman@gmail.com>
2014-03-15 19:43:44 -0600

    User signin works and added form for personal details, also works

--- app/controllers/personal_details_controller.rb
+++ app/controllers/personal_details_controller.rb
@@ -1,2 +1,29 @@
 class PersonalDetailsController < ApplicationController
+
+  def new
+    @user = current_user
+    @personal_detail = PersonalDetail.new
+  end
+
+
+  def create
+    @user = current_user
+    @personal_detail = @user.build_personal_detail(detail_params)
+
+    if @personal_detail.save
+      flash[:notice] = "Thanks for checking in! We'll track your progress"
+      redirect_to user_dashboard_path
+    else
+      flash[:notice] = "There was an error saving your info"
+      render :action => :new
+    end
+  end
+
+
+
+  private
+
+  def detail_params
+    params.require(:personal_detail).permit(:gender, :weight, :bicep, :shoulders, :chest, :neck, :waist, :hips, :thigh, :resting_heart_rate, :calf, :submit)
+  end
 end
