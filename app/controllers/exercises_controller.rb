33cda86b3808eb8a13fd4068c324065940f8ff25
Cameron Kidman <cameronjkidman@gmail.com>
2014-03-23 18:45:49 -0600

    Can now edit personal details, add new workouts, and exercises to that workout. Now need to make it happen visually

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -4,9 +4,27 @@ class ExercisesController < ApplicationController
     @workout = Workout.find(params[:workout_id])
     @exercise = Exercise.new
     @user = current_user
+    @muscle_groups = MuscleGroup.all
   end
 
   def create
+    @workout = Workout.find(params[:workout_id])
+    @exercise = @workout.exercises.build(detail_params)
 
+    if @exercise.save
+      flash[:notice] = "May I have another, SIR?!"
+      redirect_to new_workout_exercise_path(@workout)
+    else
+      flash[:notice] = "Error creating workout"
+      redirect_to user_dashboard_path
+    end
   end
+
+
+  private
+
+  def detail_params
+    params.require(:exercise).permit(:name, :youtube_url, :proper_form_text, :mini_image_url, :muscle_group, :reps, :sets, :weight, :time, :is_run)
+  end
+
 end
