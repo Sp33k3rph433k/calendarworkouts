eb250a70aa49959843472f8ae0793e71196cfac6
Cameron Kidman <cameronjkidman@gmail.com>
2014-07-05 13:49:04 -0600

    Get user avatar upload working with s3 and generate Goal model

---
+++ spec/factories/goals.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :goal do
+    name "MyString"
+    description "MyString"
+    how_text "MyString"
+  end
+end
