bec782d171c9c1c582513ba9794cb97eff1422af
Cameron Kidman <cameronjkidman@gmail.com>
2014-08-11 09:03:28 -0600

    Get wizard working properly, need to format the forms

--- app/models/initial_test.rb
+++ app/models/initial_test.rb
@@ -2,4 +2,34 @@ class InitialTest < ActiveRecord::Base
   belongs_to :user
 
 
+  # validates :basic_bodyweight,      :presence => true, :if => :active_or_basic_bodyweight?
+  # validates :one_mile,              :presence => true, :if => :active_or_one_mile?
+  # validates :gym_maxes,             :presence => true, :if => :active_or_gym_maxes?
+  #
+  # def active?
+  #   status == 'active'
+  # end
+  #
+  # def active_or_basic_bodyweight?
+  #   status.include?('basic_bodyweight') || active?
+  # end
+  #
+  # def active_or_one_mile?
+  #   status.include?('one_mile') || active?
+  # end
+  #
+  # def active_or_gym_maxes?
+  #   status.include?('gym_maxes') || active?
+  # end
+  #
+  # def complete?
+  #   status == "complete"
+  # end
+
+  private
+
+  def detail_params
+    params.require(:workout).permit(:name, :date)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140727011531) do
+ActiveRecord::Schema.define(version: 20140730022304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20140727011531) do
     t.integer  "bodyweight_squats"
     t.time     "mile_run_time"
     t.integer  "resting_heart_rate"
+    t.string   "status"
   end
 
   create_table "muscle_groups", force: true do |t|
