6265c2885c146ba758c283217a02757f51200da4
Cameron Kidman <cameronjkidman@gmail.com>
2015-07-24 16:32:45 -0600

    improve similar exercises method

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -14,11 +14,16 @@ class Exercise < ActiveRecord::Base
     muscle_groups.max_by(&:weighted_score)
   end
 
+  def secondary_muscle_group
+    sorted_muscle_groups = muscle_groups.sort { |s| s.weighted_score }
+    sorted_muscle_groups[1]
+  end
+
   def similar_exercises(muscle_group)
     lower_bound = muscle_group.weighted_score - 15
     upper_bound = muscle_group.weighted_score + 15
     # MuscleGroup.where("body_parts LIKE ?", body_parts).where("weighted_score >= ? AND weighted_score <= ?", lower_bound, upper_bound).collect(&:exercise).compact!.uniq!  commenting this out in case I need it later
-    MuscleGroup.joins(:body_parts).where(:body_parts => { part_name: ["Arms", "Legs"] }).collect(&:exercise).compact!.uniq
+    MuscleGroup.joins(:body_parts).where(:body_parts => { part_name: muscle_group.body_parts.map(&:part_name) }).collect(&:exercise).compact!.uniq
   end
 
 
