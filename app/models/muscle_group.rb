61ae9c2ea163b3e71c317477fb52e20d02b696b4
Cameron Kidman <cameronjkidman@gmail.com>
2015-07-24 17:05:34 -0600

    Minor improvements to muscle_group -- now I need to make it not duplicate body parts

--- app/models/muscle_group.rb
+++ app/models/muscle_group.rb
@@ -14,46 +14,46 @@ class MuscleGroup < ActiveRecord::Base
     case name
       when 'Abdominal'
         bps = %w(Core Hips Lower_Back)
-        bps.each {|body_part| self.body_parts.create(part_name: body_part)}
+        bps.each {|body_part| body_parts.create(part_name: body_part)}
       when 'Biceps'
         bps = %w(Arms Wrists)
-        bps.each {|body_part| self.body_parts.create(part_name: body_part)}
+        bps.each {|body_part| body_parts.create(part_name: body_part)}
       when 'Deltoids'
         bps = %w(Shoulders Arms Wrists)
-        bps.each {|body_part| self.body_parts.create(part_name: body_part)}
+        bps.each {|body_part| body_parts.create(part_name: body_part)}
       when 'Erector Spinae'
        bps = %w(Lower_Back Upper_Back Core Neck)
-       bps.each {|body_part| self.body_parts.create(part_name: body_part)}
+       bps.each {|body_part| body_parts.create(part_name: body_part)}
       when 'Gastrocnemius and Soleus'
        bps = %w(Legs Ankles Hips)
-       bps.each {|body_part| self.body_parts.create(part_name: body_part)}
+       bps.each {|body_part| body_parts.create(part_name: body_part)}
       when 'Glutes'
         bps = %w(Butt Hips Legs Ankles)
-        bps.each {|body_part| self.body_parts.create(part_name: body_part)}
+        bps.each {|body_part| body_parts.create(part_name: body_part)}
       when 'Hamstrings'
         bps = %w(Legs Ankles Butt)
-        bps.each {|body_part| self.body_parts.create(part_name: body_part)}
+        bps.each {|body_part| body_parts.create(part_name: body_part)}
       when 'Latissimus Dorsi'
         bps = %w(Upper_Back Lower_Back Neck Core)
-        bps.each {|body_part| self.body_parts.create(part_name: body_part)}
+        bps.each {|body_part| body_parts.create(part_name: body_part)}
       when 'Rhomboids'
         bps = %w(Neck Upper_Back Arms Wrists)
-        bps.each {|body_part| self.body_parts.create(part_name: body_part)}
+        bps.each {|body_part| body_parts.create(part_name: body_part)}
       when 'Obliques'
         bps = %w(Core Upper_Back Lower_Back)
-        bps.each {|body_part| self.body_parts.create(part_name: body_part)}
+        bps.each {|body_part| body_parts.create(part_name: body_part)}
       when 'Pectoralis'
         bps = %w(Arms Chest Upper_Back Shoulders)
-        bps.each {|body_part| self.body_parts.create(part_name: body_part)}
+        bps.each {|body_part| body_parts.create(part_name: body_part)}
       when 'Quadriceps'
         bps = %w(Legs Ankles Butt Hips)
-        bps.each {|body_part| self.body_parts.create(part_name: body_part)}
+        bps.each {|body_part| body_parts.create(part_name: body_part)}
       when 'Trapezius'
         bps = %w(Neck Upper_Back Arms Wrists)
-        bps.each {|body_part| self.body_parts.create(part_name: body_part)}
+        bps.each {|body_part| body_parts.create(part_name: body_part)}
       when 'Triceps'
         bps = %w(Arms Wrists Shoulders)
-        bps.each {|body_part| self.body_parts.create(part_name: body_part)}
+        bps.each {|body_part| body_parts.create(part_name: body_part)}
       else
         nil
     end
